a971ed7a05c93b5c7e8ee5d7a8726e94049f3727
Victor <victorprivv101@gmail.com>
2023-09-19 08:33:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  // Write your code here!\
+  if (ride <= 400) {
+    return "This one is on me!"
+  } else  if (ride > 2000 && ride <= 2500){
+    return "I will gladly take your thirsty bucks."
+  }else if (ride >2500) {
+    return "No can do"
+  } else {
+    return "plese write ride"
+  }
 }
+
+
+
+
+const city = "NYC"
 
 function ternaryCheckCity(){
   // Write your code here!
+  Return city === "NYC" ? "Ok, sounds good." : "No go"
 }
+
+
+const  tip = "generous"
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+      break
+      case " not as generous":
+        return "Thank you"
+        break
+        default:
+          return " bye."
+  }
 }
